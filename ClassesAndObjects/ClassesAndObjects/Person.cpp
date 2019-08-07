61fa922825ba7546347928b62d104e483a4f4988
lloydagola <lloydagola@gmail.com>
2019-08-07 07:35:57 +0600

    added Constructors and Constructor initializers

--- ClassesAndObjects/ClassesAndObjects/Person.cpp
+++ ClassesAndObjects/ClassesAndObjects/Person.cpp
@@ -1,4 +1,13 @@
 #include "Person.h"
+
+Person::Person(std::string _firstName, std::string _lastName) 
+	:
+	firstName(_firstName),
+	lastName(_lastName)
+{
+
+}
+
 std::string Person::getName() {
 	return firstName + " " + lastName;
 }
